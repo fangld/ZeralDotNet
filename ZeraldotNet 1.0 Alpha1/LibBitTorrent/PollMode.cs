069ea5e2b27f7020cf180bbd1ebc666cf6106277
comun_cp <SND\comun_cp>
2007-06-15 12:53:00 +0000

    Finished:Poll code, test and note. But don't download this version.

--- ZeraldotNet 1.0 Alpha1/LibBitTorrent/PollMode.cs
+++ ZeraldotNet 1.0 Alpha1/LibBitTorrent/PollMode.cs
@@ -5,12 +5,34 @@ using System.Text;
 
 namespace ZeraldotNet.LibBitTorrent
 {
+    /// <summary>
+    /// Poll的状态
+    /// </summary>
     public enum PollMode
     {
+        /// <summary>
+        /// 可读
+        /// </summary>
         PollIn = 1,
+
+        /// <summary>
+        /// 可写
+        /// </summary>
         PollOut = 2,
+
+        /// <summary>
+        /// 读写
+        /// </summary>
         PollInOut = 3,
+
+        /// <summary>
+        /// 错误
+        /// </summary>
         PollError = 8,
+
+        /// <summary>
+        /// 挂起
+        /// </summary>
         PollHangUp = 16
     }
 }
