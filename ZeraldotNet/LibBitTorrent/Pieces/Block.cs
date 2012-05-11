d37acae2e3db3209a34d0a898ee66ab313ff5539
comun_cp <SND\comun_cp>
2012-05-11 15:02:11 +0000

    Daily Commit(2012-5-11) +:Add download block codes.

---
+++ ZeraldotNet/LibBitTorrent/Pieces/Block.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeraldotNet.LibBitTorrent.Pieces
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Block
+    {
+        #region Properties
+
+        public int Begin { get; set; }
+
+        public int Length { get; set; }
+
+        #endregion
+    }
+}
