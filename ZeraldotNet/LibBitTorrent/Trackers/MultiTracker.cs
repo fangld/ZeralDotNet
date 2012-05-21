1a97f0122da390f66b1ee0c377edb630157dc34e
comun_cp <SND\comun_cp>
2012-05-21 14:05:04 +0000

    Daily Commit(2012-5-21) +:Support multi tracker.

---
+++ ZeraldotNet/LibBitTorrent/Trackers/MultiTracker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZeraldotNet.LibBitTorrent.BEncoding;
+
+namespace ZeraldotNet.LibBitTorrent.Trackers
+{
+    public class MultiTracker
+    {
+        #region Fields
+
+        private AnnounceRequest _request;
+
+        private List<LinkedList<Tracker>> _trackers;
+
+        #endregion
+
+        #region Constructors
+
+        public MultiTracker(AnnounceRequest request)
+        {
+            _request = request;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Parse(MetaInfo metaInfo)
+        {
+            _trackers = new List<LinkedList<Tracker>>(metaInfo.AnnounceArrayListCount);
+            for (int i = 0; i < metaInfo.AnnounceArrayListCount; i++)
+            {
+                LinkedList<Tracker> trackerList = new LinkedList<Tracker>();
+                IList<string> announceList = metaInfo.GetAnnounceList(i);
+                for (int j = 0; j < announceList.Count; j++)
+                {
+                    Tracker tracker = new Tracker(_request);
+                    tracker.Url = announceList[j];
+                    trackerList.AddLast(tracker);
+                }
+                _trackers.Add(trackerList);
+            }
+        }
+
+        public void Announce()
+        {
+            for (int i = 0; i < _trackers.Count; i++)
+            {
+                
+            }
+        }
+
+        #endregion
+    }
+}
